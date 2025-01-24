5e12e366ee98ed159a1d0f4eb2b39ad69b5a9ff2
Fredrik Svard <haffy@icloud.com>
2025-01-24 12:14:35 +0100

    Added mongodb init file.

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+﻿db = db.getSiblingDB('admin');
+db.auth('admin', 'securepassword');
+
+db = db.getSiblingDB('tbas_db');
+db.createUser({
+    user: "appuser",
+    pwd: "apppassword",
+    roles: [
+        { role: "dbOwner", db: "tbas_db" },
+        { role: "readWrite", db: "tbas_db" }
+    ]
+});
