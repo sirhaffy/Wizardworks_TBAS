2ac2f45a9f71c1cc91136e6539285ecbd437a19d
sirhaffy <haffy@icloud.com>
2025-02-13 15:22:29 +0100

    Added SSH public key file

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -90,7 +90,7 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   admin_ssh_key {
     username   = var.admin_username
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/tbas_deploy_key.pub")
   }
 
   os_disk {
